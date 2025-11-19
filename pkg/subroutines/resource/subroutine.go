df9b1466e17f436d9edc8e231597ce1438e6e814
Bastian Echterhölter <bastian.echterhoelter@sap.com>
2025-11-19 09:33:07 +0100

    feat: adds a controller to reconcile the OCM Resource kind and to update respective flux resources (#99)
    * feat(resource): implement ResourceReconciler and associated subroutines for managing OCI and Helm repositories
    
    Signed-off-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>
    On-behalf-of: @SAP <bastian.echterhoelter@sap.com>
    
    * feat(resource): implement ResourceReconciler and associated subroutines for managing OCI and Helm repositories
    
    Signed-off-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>
    On-behalf-of: @SAP <bastian.echterhoelter@sap.com>
    
    * feat(resource): implement ResourceReconciler and associated subroutines for managing OCI and Helm repositories
    
    Signed-off-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>
    On-behalf-of: @SAP <bastian.echterhoelter@sap.com>
    
    * fix(resource): return error on failure to create or update OCIRepository
    
    Signed-off-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>
    On-behalf-of: @SAP <bastian.echterhoelter@sap.com>
    
    * fix: also process git repos
    
    * update testenv for resource subroutine
    
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>
    
    ---------
    
    Signed-off-by: Bastian Echterhölter <bastian.echterhoelter@sap.com>
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>
    Co-authored-by: aaronschweig <aaron.schweig@gmail.com>
    Co-authored-by: Angel Kafazov <akafazov@cst-bg.net>

---
+++ pkg/subroutines/resource/subroutine.go
@@ -0,0 +1,295 @@
+package resource
+
+import (
+	"context"
+	"strings"
+
+	"github.com/platform-mesh/golang-commons/controller/lifecycle/runtimeobject"
+	"github.com/platform-mesh/golang-commons/errors"
+	"github.com/platform-mesh/golang-commons/logger"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var ociRepoGvk = schema.GroupVersionKind{
+	Group:   "source.toolkit.fluxcd.io",
+	Version: "v1",
+	Kind:    "OCIRepository",
+}
+
+var gitRepoGvk = schema.GroupVersionKind{
+	Group:   "source.toolkit.fluxcd.io",
+	Version: "v1",
+	Kind:    "GitRepository",
+}
+
+var helmRepoGvk = schema.GroupVersionKind{
+	Group:   "source.toolkit.fluxcd.io",
+	Version: "v1",
+	Kind:    "HelmRepository",
+}
+
+var helmReleaseGvk = schema.GroupVersionKind{
+	Group:   "helm.toolkit.fluxcd.io",
+	Version: "v2",
+	Kind:    "HelmRelease",
+}
+
+type ResourceSubroutine struct {
+	mgr manager.Manager
+}
+
+func NewResourceSubroutine(mgr manager.Manager) *ResourceSubroutine {
+	return &ResourceSubroutine{mgr: mgr}
+}
+
+func (r *ResourceSubroutine) GetName() string {
+	return "ResourceSubroutine"
+}
+
+func (r *ResourceSubroutine) Finalize(_ context.Context, _ runtimeobject.RuntimeObject) (ctrl.Result, errors.OperatorError) {
+	return ctrl.Result{}, nil
+}
+
+func (r *ResourceSubroutine) Finalizers() []string { // coverage-ignore
+	return []string{}
+}
+
+func (r *ResourceSubroutine) Process(ctx context.Context, runtimeObj runtimeobject.RuntimeObject) (ctrl.Result, errors.OperatorError) {
+	inst := runtimeObj.(*unstructured.Unstructured)
+	log := logger.LoadLoggerFromContext(ctx).ChildLogger("name", r.GetName())
+
+	repo := inst.GetLabels()["repo"]
+	artifact := inst.GetLabels()["artifact"]
+
+	if repo == "oci" && artifact == "chart" {
+		log.Debug().Msg("Create/Update OCI Repo")
+		result, err := r.updateOciRepo(ctx, inst, log)
+		if err != nil {
+			return result, err
+		}
+	}
+	if repo == "git" && artifact == "chart" {
+		log.Debug().Msg("Create/Update Git Repo")
+		result, err := r.updateGitRepo(ctx, inst, log)
+		if err != nil {
+			return result, err
+		}
+	}
+	if repo == "helm" && artifact == "chart" {
+		log.Debug().Msg("Create/Update Flux Helm Repository Repo")
+		result, err := r.updateHelmRepository(ctx, inst, log)
+		if err != nil {
+			return result, err
+		}
+		log.Debug().Msg("Update Flux Helm Release Repo")
+		result, err = r.updateHelmRelease(ctx, inst, log)
+		if err != nil {
+			return result, err
+		}
+	}
+	if repo == "helm" && artifact == "image" {
+		log.Debug().Msg("Update Helm Release with Image Tag")
+		result, err := r.updateHelmReleaseWithImageTag(ctx, inst, log)
+		if err != nil {
+			return result, err
+		}
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *ResourceSubroutine) updateHelmReleaseWithImageTag(ctx context.Context, inst *unstructured.Unstructured, log *logger.Logger) (ctrl.Result, errors.OperatorError) {
+	obj := &unstructured.Unstructured{}
+	obj.SetGroupVersionKind(helmReleaseGvk)
+	obj.SetName(inst.GetName())
+	obj.SetNamespace(inst.GetNamespace())
+
+	version, found, err := unstructured.NestedString(inst.Object, "status", "resource", "version")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get version from Resource status")
+	}
+
+	err = r.mgr.GetClient().Get(ctx, client.ObjectKeyFromObject(inst), obj)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get HelmRelease")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+
+	err = unstructured.SetNestedField(obj.Object, version, "spec", "values", "image", "tag")
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to set version in HelmRelease spec")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, false)
+	}
+
+	err = r.mgr.GetClient().Update(ctx, obj)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to update HelmRelease")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *ResourceSubroutine) updateHelmRelease(ctx context.Context, inst *unstructured.Unstructured, log *logger.Logger) (ctrl.Result, errors.OperatorError) {
+	obj := &unstructured.Unstructured{}
+	obj.SetGroupVersionKind(helmReleaseGvk)
+	obj.SetName(inst.GetName())
+	obj.SetNamespace(inst.GetNamespace())
+
+	version, found, err := unstructured.NestedString(inst.Object, "status", "resource", "version")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get version from Resource status")
+	}
+
+	err = r.mgr.GetClient().Get(ctx, client.ObjectKeyFromObject(inst), obj)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get HelmRelease")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+
+	err = unstructured.SetNestedField(obj.Object, version, "spec", "chart", "spec", "version")
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to set version in HelmRelease spec")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, false)
+	}
+
+	err = r.mgr.GetClient().Update(ctx, obj)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to update HelmRelease")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *ResourceSubroutine) updateHelmRepository(ctx context.Context, inst *unstructured.Unstructured, log *logger.Logger) (ctrl.Result, errors.OperatorError) {
+	url, found, err := unstructured.NestedString(inst.Object, "status", "resource", "access", "helmRepository")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get imageReference from Resource status")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, false)
+	}
+
+	log.Info().Msg("Processing OCI Chart Resource")
+	obj := &unstructured.Unstructured{}
+	obj.SetGroupVersionKind(helmRepoGvk)
+	obj.SetName(inst.GetName())
+	obj.SetNamespace(inst.GetNamespace())
+	_, err = controllerutil.CreateOrUpdate(ctx, r.mgr.GetClient(), obj, func() error {
+		err := unstructured.SetNestedField(obj.Object, url, "spec", "url")
+		if err != nil {
+			return err
+		}
+		err = unstructured.SetNestedField(obj.Object, "generic", "spec", "provider")
+		if err != nil {
+			return err
+		}
+		err = unstructured.SetNestedField(obj.Object, "5m", "spec", "interval")
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create or update OCIRepository")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *ResourceSubroutine) updateOciRepo(ctx context.Context, inst *unstructured.Unstructured, log *logger.Logger) (ctrl.Result, errors.OperatorError) {
+	version, found, err := unstructured.NestedString(inst.Object, "status", "resource", "version")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get version from Resource status")
+	}
+	url, found, err := unstructured.NestedString(inst.Object, "status", "resource", "access", "imageReference")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get imageReference from Resource status")
+	}
+
+	url = strings.TrimPrefix(url, "oci://")
+
+	url = "oci://" + url
+	url = strings.TrimSuffix(url, ":"+version)
+
+	// Update or create oci repo
+	log.Info().Msg("Processing OCI Chart Resource")
+	obj := &unstructured.Unstructured{}
+	obj.SetGroupVersionKind(ociRepoGvk)
+	obj.SetName(inst.GetName())
+	obj.SetNamespace(inst.GetNamespace())
+	_, err = controllerutil.CreateOrUpdate(ctx, r.mgr.GetClient(), obj, func() error {
+		err := unstructured.SetNestedField(obj.Object, version, "spec", "ref", "tag")
+		if err != nil {
+			return err
+		}
+		err = unstructured.SetNestedField(obj.Object, url, "spec", "url")
+		if err != nil {
+			return err
+		}
+		err = unstructured.SetNestedField(obj.Object, "generic", "spec", "provider")
+		if err != nil {
+			return err
+		}
+		err = unstructured.SetNestedField(obj.Object, "1m0s", "spec", "interval")
+		if err != nil {
+			return err
+		}
+		err = unstructured.SetNestedMap(inst.Object, map[string]interface{}{
+			"mediaType": "application/vnd.cncf.helm.chart.content.v1.tar+gzip",
+			"operation": "copy",
+		}, "spec", "layerSelector")
+		return err
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create or update OCIRepository")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+	return ctrl.Result{}, nil
+}
+
+func (r *ResourceSubroutine) updateGitRepo(ctx context.Context, inst *unstructured.Unstructured, log *logger.Logger) (ctrl.Result, errors.OperatorError) {
+	commit, found, err := unstructured.NestedString(inst.Object, "status", "resource", "access", "commit")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get version from Resource status")
+	}
+
+	url, found, err := unstructured.NestedString(inst.Object, "status", "resource", "access", "repoUrl")
+	if err != nil || !found {
+		log.Info().Err(err).Msg("Failed to get imageReference from Resource status")
+	}
+
+	// Update or create oci repo
+	log.Info().Msg("Processing OCI Chart Resource")
+	obj := &unstructured.Unstructured{}
+
+	obj.SetGroupVersionKind(gitRepoGvk)
+	obj.SetName(inst.GetName())
+	obj.SetNamespace(inst.GetNamespace())
+
+	_, err = controllerutil.CreateOrUpdate(ctx, r.mgr.GetClient(), obj, func() error {
+
+		err := unstructured.SetNestedField(obj.Object, commit, "spec", "ref", "commit")
+		if err != nil {
+			return err
+		}
+
+		err = unstructured.SetNestedField(obj.Object, url, "spec", "url")
+		if err != nil {
+			return err
+		}
+
+		err = unstructured.SetNestedField(obj.Object, "1m0s", "spec", "interval")
+		if err != nil {
+			return err
+		}
+
+		return err
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create or update OCIRepository")
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+	return ctrl.Result{}, nil
+}
