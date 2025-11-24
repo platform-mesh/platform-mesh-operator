c42f341dd34f35287467898ad40edc2fc5264e90
Angel Kafazov <akafazov@cst-bg.net>
2025-11-24 22:08:58 +0200

    comment out TestRunFor30Minutes in KindTestSuite
    On-behalf-of: @SAP angel.kafazov@sap.com
    Signed-off-by: Angel Kafazov <akafazov@cst-bg.net>

--- test/e2e/kind/kind_operator_test.go
+++ test/e2e/kind/kind_operator_test.go
@@ -41,11 +41,11 @@ func (s *KindTestSuite) TestResourceReady() {
 
 }
 
-func (s *KindTestSuite) TestRunFor30Minutes() {
-	s.Eventually(func() bool {
-		return false
-	}, 30*time.Minute, 10*time.Second)
-}
+// func (s *KindTestSuite) TestRunFor30Minutes() {
+// 	s.Eventually(func() bool {
+// 		return false
+// 	}, 30*time.Minute, 10*time.Second)
+// }
 
 // func (s *KindTestSuite) TestExtraWorkspaces() {
 // 	ctx := context.TODO()
