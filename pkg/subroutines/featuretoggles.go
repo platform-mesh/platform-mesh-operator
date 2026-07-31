74f3eb10f185cd4ce794dc1753fbf3913d98607f
OlegErshov <olezhka1629@gmail.com>
2026-07-31 14:20:35 +0200

    chore: rename provider permissions feature
    Signed-off-by: OlegErshov <olezhka1629@gmail.com>
    
    On-behalf-of: SAP aleh.yarshou@sap.com

--- pkg/subroutines/featuretoggles.go
+++ pkg/subroutines/featuretoggles.go
@@ -121,8 +121,8 @@ func (r *FeatureToggleSubroutine) Process(ctx context.Context, runtimeObj client
 				return subroutines.OK(), applyErr
 			}
 			log.Info().Msg("Enabled 'Terminal controller manager' feature")
-		case "feature-provider-permissions":
-			_, applyErr := r.applyKcpManifests(ctx, inst, operatorCfg, "/feature-provider-permissions")
+		case "feature-enable-provider-permissions":
+			_, applyErr := r.applyKcpManifests(ctx, inst, operatorCfg, "/feature-enable-provider-permissions")
 			if applyErr != nil {
 				log.Error().Err(applyErr).Msg("Failed to apply provider-permissions manifests")
 				return subroutines.OK(), applyErr
